e4ac5f1be24c1f66321a66335c2b7212373b4c54
jalexpr <32515097+jalexpr@users.noreply.github.com>
2017-10-09 18:26:44 +0300

    Добавлены методы addLength, addAll и т.д.
    Добавлены методы addLength, addAll, findFirst, findLast, indexOf и remove у Iterator()

--- src/myarraylist/MyArrayList.java
+++ src/myarraylist/MyArrayList.java
@@ -1,81 +1,178 @@
-package myarraylist;
+package mycollections;
 
 import java.util.Iterator;
 
-public class MyArrayList implements Iterable {
+public class MyArrayList implements Iterable, MyList {
 
     private int size;
-    private String arr[];
+    private Object arr[];
 
     public MyArrayList() {
         this(5);
     }
 
     public MyArrayList(int length) {
+
         this.size = 0;
-        this.arr = new String[length];
+        this.arr = new Object[length];
     }
 
-    public boolean add(String element) {
-        
+    //увеличение длинны доступного массива на размер по умолчанию
+    private void addLength() {
+        addLength((arr.length * 3) / 2);
+    }
+
+    //увеличение длинны доступного массива на заданный размер
+    private void addLength(int addLength) {
+
+        Object tempArr[] = new Object[arr.length + addLength];
+        System.arraycopy(arr, 0, tempArr, 0, size);
+        arr = tempArr;
+    }
+
+    @Override
+    public boolean add(Object element) {
+
         if (size >= arr.length) {
-
-            String tempArr[] = new String[arr.length + arr.length / 2 + 1];
-            System.arraycopy(arr, 0, tempArr, 0, size);
-            arr = tempArr;
-
+            addLength();
         }
 
         arr[size++] = element;
         return true;
-        
     }
 
-    public String get(int index) throws IndexOutOfBoundsException {
-        if (index < size) {
+    @Override
+    public boolean add(int index, Object element) {
+
+        if (size >= arr.length) {
+            addLength();
+        }
+        
+        
+        System.arraycopy(arr, index, arr, index + 1, size - index);
+
+        arr[index] = element;
+
+        size++;
+
+        return true;
+    }
+
+    @Override
+    public boolean addAll(MyList source) {
+        return addAll(0, source);
+    }
+
+    @Override
+    public boolean addAll(int start, MyList source) {
+        
+        Object[] arrSource = ((MyArrayList) source).arr;
+        int sourcelength = arrSource.length;
+        int newSize = size + ((MyArrayList) source).size() - start;
+
+        if (newSize > arr.length) {
+            addLength(sourcelength);
+        }
+
+        System.arraycopy(arrSource, start, arr, size,  sourcelength - start);
+
+        size = newSize;
+
+        return true;
+    }
+
+    @Override
+    public Object get(int index) throws IndexOutOfBoundsException {
+
+        if (index > -1 && index < size && !isEmpty()) {
             return arr[index];
-        }else {
+        } else {
             throw new IndexOutOfBoundsException("End of list.");
         }
     }
 
-    public int indexOf(String element) {
-        
-        for (int i = 0; i < size; i++) {
-            
-            if (arr[i].equals(element)) {
-                return i;
+    @Override
+    public Object findFirst() {
+        if (!isEmpty()) {
+            return arr[0];
+        } else {
+            throw new IndexOutOfBoundsException();
+        }
+    }
+
+    @Override
+    public Object findLast() {
+        if (!isEmpty()) {
+            return arr[size - 1];
+        } else {
+            throw new IndexOutOfBoundsException();
+        }
+    }
+
+    @Override
+    public int indexOf(Object element) {
+        if (!isEmpty()) {
+            for (int i = 0; i < size; i++) {
+                if (arr[i].equals(element)) {
+                    return i;
+                }
             }
-            
+        } else {
+            throw new IndexOutOfBoundsException();
         }
-        
         return -1;
-        
     }
 
-    public String remove(int index) throws IndexOutOfBoundsException {
-
-        String tempElement = null;
-
-        if (index < size) {
-            tempElement = arr[index];
-            System.arraycopy(arr, index + 1, arr, index, size - 1 - index);
-            size--;
-        } else {
-            throw new IndexOutOfBoundsException("End of list.");
-        }
-
-        return tempElement; 
-    }
-
-    public String set(int index, String element) {
-        if (index < size) {
-            String tempStr = arr[index];
+    @Override
+    public boolean set(int index, Object element) {
+        if (index > -1 && index < size && !isEmpty()) {
             arr[index] = element;
-            return tempStr;
+            return true;
         } else {
-            throw new IndexOutOfBoundsException("End of list.");
+            return false;
         }
+    }
+
+    @Override
+    public Object remove(int index) {
+
+        Object tempElement = null;
+
+        if (index > -1) {
+            if (index < size) {
+                tempElement = arr[index];
+                System.arraycopy(arr, index + 1, arr, index, size - 1 - index);
+                size--;
+            } else {
+                throw new IndexOutOfBoundsException("End of list.");
+            }
+        }
+
+        return tempElement;
+    }
+
+    @Override
+    public boolean remove(Object o) {
+        return remove(indexOf(o)) != null;
+    }
+
+    @Override
+    public boolean removeAll() {
+
+        arr = new Object[arr.length];
+        size = 0;
+
+        return true;
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return size == 0;
     }
 
     @Override
@@ -90,13 +187,18 @@ public class MyArrayList implements Iterable {
             }
 
             @Override
-            public String next() {
-                String result = arr[current];
+            public Object next() {
+                Object result = arr[current];
                 if (!hasNext()) {
                     throw new IndexOutOfBoundsException("End of list.");
                 }
                 current++;
                 return result;
+            }
+
+            @Override
+            public void remove() {
+                MyArrayList.this.remove(current);
             }
         };
     }
