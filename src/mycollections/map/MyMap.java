daabe0f7bbb08f43715420c984a7b96bfdc8f648
jalexpr <32515097+jalexpr@users.noreply.github.com>
2017-10-30 10:18:53 +0300

    Удален интерфейс через итерацию

--- src/mycollections/map/MyMap.java
+++ src/mycollections/map/MyMap.java
@@ -7,25 +7,19 @@ public interface MyMap {
 
     public boolean isEmpty();
 
-    public boolean containsKey(Object o);
+    public boolean containsKey(Object key);
 
-    public boolean containsValue(Object o);
+    public boolean containsValue(Object value);
 
-    public Object get(Object o);
+    public Object get(Object key);
 
-    public Object put(Object k, Object v);
+    public Object put(Object key, Object value);
 
-    public Object remove(Object o);
+    public Object remove(Object key);
 
     public void putAll(MyMap map);
 
     public void clear();
-
-    //public Set keySet();
-
-    //public Collection values();
-
-    //public Set entrySet();
     
-    //public boolean equals(MyMap o);
+    public MyMap getEmptyMyMap();
 }
